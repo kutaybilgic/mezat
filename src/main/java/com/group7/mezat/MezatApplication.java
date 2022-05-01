d0d936d527dadd9b890224f790fccaaa7e2c5b8b
Çağdai <cag_das98@hotmail.com>
2022-05-02 01:31:50 +0300

    initilize

---
+++ src/main/java/com/group7/mezat/MezatApplication.java
@@ -0,0 +1,13 @@
+package com.group7.mezat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MezatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MezatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group7/mezat/MezatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.mezat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MezatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
